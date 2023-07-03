f2201876b553e4d6f0f4fed853ceb51f9ba11210
CHAN YIXIN <22038283@myrp.edu.sg>
2023-07-03 18:13:50 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22038283/songps/Song.java
@@ -0,0 +1,45 @@
+package sg.edu.rp.c346.id22038283.songps;
+
+import androidx.annotation.NonNull;
+
+public class Song {
+    private int id;
+    private String title;
+    private String singers;
+    private int year;
+    private int star;
+
+    public Song(String title, String singers, int year, int star) {
+        this.id = id;
+        this.title = title;
+        this.singers = singers;
+        this.year = year;
+        this.star = star;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return id + "\n" + title + "\n" + singers + "\n" + year + "\n" + star;
+    }
+}
